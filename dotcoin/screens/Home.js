11dec675ecceafed1e834c34ab95de56de0768e3
Shaima-Cloud99X <143798888+Shaima-Cloud99X@users.noreply.github.com>
2024-02-17 06:57:01 +0400

    Home trending

--- dotcoin/screens/Home.js
+++ dotcoin/screens/Home.js
@@ -1,13 +1,178 @@
 
 import React from 'react';
-import { View, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { View, TouchableOpacity, Image, StyleSheet, Text, } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const Home = () => {
   const navigation = useNavigation();
 
   return (
     <View style={styles.mainContainer}>
+        <View style = {styles.container1}>
+          <Text style= {styles.title} >Home</Text>
+        </View>
+        <ScrollView>
+          <View style= {styles.container2}>
+          <Text style= {styles.title2} >Trending</Text>
+          <View style = {{marginTop:20}}> 
+            <View style = {styles.row}>
+            <Image source={require('../assets/bitcoinIcon.png')} style = {{width:70, height:70}}  />
+            <View>
+            <Text style = {styles.text1}>Bitcoin</Text>
+            <Text style = {styles.text2}>BTC</Text>
+            </View>
+            <Image source={require('../assets/Graph1.png')} style = {{marginTop:20, marginLeft:25}}  />
+            <View>
+            <Text style = {styles.text3}>$41,192.14</Text>
+            <View style = {{flexDirection:'row', marginLeft:15}}>
+            <Image source={require('../assets/chevronOrange.png')} style = {{marginLeft:25, marginTop:2}}  />
+            <Text style = {styles.text4}>0.55%</Text>
+            </View>
+            </View>
+            </View>
+            <View style={styles.divider} />
+            <View style = {styles.row}>
+            <Image source={require('../assets/bitcoinIcon.png')} style = {{width:70, height:70}}  />
+            <View>
+            <Text style = {styles.text1}>Bitcoin</Text>
+            <Text style = {styles.text2}>BTC</Text>
+            </View>
+            <Image source={require('../assets/Graph1.png')} style = {{marginTop:20, marginLeft:25}}  />
+            <View>
+            <Text style = {styles.text3}>$41,192.14</Text>
+            <View style = {{flexDirection:'row', marginLeft:15}}>
+            <Image source={require('../assets/chevronOrange.png')} style = {{marginLeft:25, marginTop:2}}  />
+            <Text style = {styles.text4}>0.55%</Text>
+            </View>
+            </View>
+            </View>
+            <View style={styles.divider} />
+
+
+            <View style = {styles.row}>
+            <Image source={require('../assets/bitcoinIcon.png')} style = {{width:70, height:70}}  />
+            <View>
+            <Text style = {styles.text1}>Bitcoin</Text>
+            <Text style = {styles.text2}>BTC</Text>
+            </View>
+            <Image source={require('../assets/Graph1.png')} style = {{marginTop:20, marginLeft:25}}  />
+            <View>
+            <Text style = {styles.text3}>$41,192.14</Text>
+            <View style = {{flexDirection:'row', marginLeft:15}}>
+            <Image source={require('../assets/chevronOrange.png')} style = {{marginLeft:25, marginTop:2}}  />
+            <Text style = {styles.text4}>0.55%</Text>
+            </View>
+            </View>
+            </View>
+            <View style={styles.divider} />
+
+
+            <View style = {styles.row}>
+            <Image source={require('../assets/bitcoinIcon.png')} style = {{width:70, height:70}}  />
+            <View>
+            <Text style = {styles.text1}>Bitcoin</Text>
+            <Text style = {styles.text2}>BTC</Text>
+            </View>
+            <Image source={require('../assets/Graph1.png')} style = {{marginTop:20, marginLeft:25}}  />
+            <View>
+            <Text style = {styles.text3}>$41,192.14</Text>
+            <View style = {{flexDirection:'row', marginLeft:15}}>
+            <Image source={require('../assets/chevronOrange.png')} style = {{marginLeft:25, marginTop:2}}  />
+            <Text style = {styles.text4}>0.55%</Text>
+            </View>
+            </View>
+            </View>
+            <View style={styles.divider} />
+
+
+            <View style = {styles.row}>
+            <Image source={require('../assets/bitcoinIcon.png')} style = {{width:70, height:70}}  />
+            <View>
+            <Text style = {styles.text1}>Bitcoin</Text>
+            <Text style = {styles.text2}>BTC</Text>
+            </View>
+            <Image source={require('../assets/Graph1.png')} style = {{marginTop:20, marginLeft:25}}  />
+            <View>
+            <Text style = {styles.text3}>$41,192.14</Text>
+            <View style = {{flexDirection:'row', marginLeft:15}}>
+            <Image source={require('../assets/chevronOrange.png')} style = {{marginLeft:25, marginTop:2}}  />
+            <Text style = {styles.text4}>0.55%</Text>
+            </View>
+            </View>
+            </View>
+            <View style={styles.divider} />
+
+            <View style = {styles.row}>
+            <Image source={require('../assets/bitcoinIcon.png')} style = {{width:70, height:70}}  />
+            <View>
+            <Text style = {styles.text1}>Bitcoin</Text>
+            <Text style = {styles.text2}>BTC</Text>
+            </View>
+            <Image source={require('../assets/Graph1.png')} style = {{marginTop:20, marginLeft:25}}  />
+            <View>
+            <Text style = {styles.text3}>$41,192.14</Text>
+            <View style = {{flexDirection:'row', marginLeft:15}}>
+            <Image source={require('../assets/chevronOrange.png')} style = {{marginLeft:25, marginTop:2}}  />
+            <Text style = {styles.text4}>0.55%</Text>
+            </View>
+            </View>
+            </View>
+            <View style={styles.divider} />
+
+
+            <View style = {styles.row}>
+            <Image source={require('../assets/bitcoinIcon.png')} style = {{width:70, height:70}}  />
+            <View>
+            <Text style = {styles.text1}>Bitcoin</Text>
+            <Text style = {styles.text2}>BTC</Text>
+            </View>
+            <Image source={require('../assets/Graph1.png')} style = {{marginTop:20, marginLeft:25}}  />
+            <View>
+            <Text style = {styles.text3}>$41,192.14</Text>
+            <View style = {{flexDirection:'row', marginLeft:15}}>
+            <Image source={require('../assets/chevronOrange.png')} style = {{marginLeft:25, marginTop:2}}  />
+            <Text style = {styles.text4}>0.55%</Text>
+            </View>
+            </View>
+            </View>
+            <View style={styles.divider} />
+
+
+            <View style = {styles.row}>
+            <Image source={require('../assets/bitcoinIcon.png')} style = {{width:70, height:70}}  />
+            <View>
+            <Text style = {styles.text1}>Bitcoin</Text>
+            <Text style = {styles.text2}>BTC</Text>
+            </View>
+            <Image source={require('../assets/Graph1.png')} style = {{marginTop:20, marginLeft:25}}  />
+            <View>
+            <Text style = {styles.text3}>$41,192.14</Text>
+            <View style = {{flexDirection:'row', marginLeft:15}}>
+            <Image source={require('../assets/chevronOrange.png')} style = {{marginLeft:25, marginTop:2}}  />
+            <Text style = {styles.text4}>0.55%</Text>
+            </View>
+            </View>
+            </View>
+            <View style={styles.divider} />
+
+          </View>
+
+          </View>
+        </ScrollView>
+   
+
+
+
+
+
+
+
+
+
+
+
+
       <View style={styles.container}>
         <TouchableOpacity
           style={styles.tab}
@@ -51,6 +216,78 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#000000',
   },
+
+  container1: {
+    margin:20
+
+  },
+
+  divider: {
+    borderBottomColor: '#404040',
+    borderBottomWidth: 1,
+    marginVertical: 10,
+  },
+
+  container2: {
+    margin:20
+
+  },
+
+  row: {
+    flexDirection:'row'
+
+  },
+
+  title: {
+    color: '#fff',
+    fontFamily: 'Poppins-Medium',
+    fontSize: 27,
+    
+  },
+
+  text1: {
+    color: '#fff',
+    fontFamily: 'Poppins-Medium',
+    fontSize: 18,
+    marginLeft:5,
+    marginTop:10
+    
+  },
+
+  text2: {
+    color: '#808D90',
+    fontFamily: 'Poppins-Medium',
+    fontSize: 18,
+    marginLeft:5,
+   
+    
+  },
+
+  text3: {
+    color: '#fff',
+    fontFamily: 'Poppins-Medium',
+    fontSize: 18,
+    marginLeft:25,
+    marginTop:10
+    
+  },
+
+  text4: {
+    color: '#808D90',
+    fontFamily: 'Poppins-Medium',
+    fontSize: 18,
+    marginLeft:6,
+   
+    
+  },
+
+  title2: {
+    color: '#fff',
+    fontFamily: 'Poppins-Medium',
+    fontSize: 21,
+    
+  },
+
   container: {
     flexDirection: 'row',
     justifyContent: 'space-around',
